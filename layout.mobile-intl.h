5873f6f7dad4f98999ab18f14aa23e22d468b4d6
p4p1 <p4p1@vivaldi.net>
2023-12-19 00:19:22 +0100

    set to orange theme

--- layout.mobile-intl.h
+++ layout.mobile-intl.h
@@ -1,4 +1,4 @@
-#define KEYS 57
+#define KEYS 58
 
 static Key keys_en[KEYS] = {
 	{ "Esc", "", XK_Escape, 1 },
@@ -61,7 +61,8 @@ static Key keys_en[KEYS] = {
 	{ 0 }, /* New row */
 	{ "↺", 0, XK_Cancel, 1},
 	{ "Alt", 0, XK_Alt_L, 1 },
-	{ "", 0, XK_space, 4 },
+	{ "Ctr", 0, XK_Control_L, 1 },
+	{ "[ ]", 0, XK_space, 4 },
 	{ "↓", 0, XK_Down, 1 },
 	{ "↑", 0, XK_Up, 1 },
 	{ "↲ Enter", 0, XK_Return, 2 },
@@ -475,70 +476,6 @@ static Key keys_navigation[KEYS] = {
 };
 
 
-static Key keys_ru[KEYS] = {
-	{ "Esc", 0, XK_Escape, 1 },
-	{ "1", "!", XK_1, 1 },
-	{ "2", "@", XK_2, 1 },
-	{ "3", "#", XK_3, 1 },
-	{ "4", "$", XK_4, 1 },
-	{ "5", "%", XK_5, 1 },
-	{ "6", "^", XK_6, 1 },
-	{ "7", "&", XK_7, 1 },
-	{ "8", "*", XK_8, 1 },
-	{ "9", "(", XK_9, 1 },
-	{ "0", ")", XK_0, 1 },
-	{ 0 }, /* New row */
-
-	{ "й", 0, XK_Cyrillic_shorti, 1 },
-	{ "ц", 0, XK_Cyrillic_tse, 1 },
-	{ "у", 0, XK_Cyrillic_u, 1 },
-	{ "к", 0, XK_Cyrillic_ka, 1 },
-	{ "е", 0, XK_Cyrillic_ie, 1 },
-	{ "н", 0, XK_Cyrillic_en, 1 },
-	{ "г", 0, XK_Cyrillic_ghe, 1 },
-	{ "ш", 0, XK_Cyrillic_sha, 1 },
-	{ "щ", 0, XK_Cyrillic_shcha, 1 },
-	{ "з", 0, XK_Cyrillic_ze, 1 },
-	{ "х", 0, XK_Cyrillic_ha, 1 },
-
-	{ 0 }, /* New row */
-
-	{ "ф", 0, XK_Cyrillic_ef, 1 },
-	{ "ы", 0, XK_Cyrillic_yeru, 1 },
-	{ "в", 0, XK_Cyrillic_ve, 1 },
-	{ "а", 0, XK_Cyrillic_a, 1 },
-	{ "п", 0, XK_Cyrillic_pe, 1 },
-	{ "р", 0, XK_Cyrillic_er, 1 },
-	{ "о", 0, XK_Cyrillic_o, 1 },
-	{ "л", 0, XK_Cyrillic_el, 1 },
-	{ "д", 0, XK_Cyrillic_de, 1 },
-	{ "ж", 0, XK_Cyrillic_zhe, 1 },
-	{ "ю", 0, XK_Cyrillic_yu, 1 },
-
-	{ 0 }, /* New row */
-
-	{ "123", 0, XK_Mode_switch, 1 },
-	{ "я", 0, XK_Cyrillic_ya, 1 },
-	{ "ч", 0, XK_Cyrillic_che, 1 },
-	{ "с", 0, XK_Cyrillic_es, 1 },
-	{ "м", 0, XK_Cyrillic_em, 1 },
-	{ "и", 0, XK_Cyrillic_i, 1 },
-	{ "т", 0, XK_Cyrillic_te, 1 },
-	{ "ь", 0, XK_Cyrillic_softsign, 1 },
-	{ "б", 0, XK_Cyrillic_be, 1 },
-	{ "⌫Bksp", 0, XK_BackSpace, 2 },
-
-	{ 0 }, /* New row */
-	{ "↺", 0, XK_Cancel, 1},
-	{ "Shift", 0, XK_Shift_L, 2 },
-	{ "Ctrl", 0, XK_Control_L, 1 },
-	{ "Alt", 0, XK_Alt_L, 1 },
-	{ "", 0, XK_space, 2 },
-	{ "↓", 0, XK_Down, 1 },
-	{ "↑", 0, XK_Up, 1 },
-	{ "↲ Enter", 0, XK_Return, 2 },
-};
-
 static Key keys_dialer[KEYS] = {
 	{ "Esc", 0, XK_Escape, 1 },
 	{ "1", "!" , XK_1, 1 },
@@ -572,14 +509,13 @@ static Key keys_dialer[KEYS] = {
 	{ 0 }, /* Last item (double 0) */
 };
 
-#define LAYERS 6
+#define LAYERS 5
 static char* layer_names[LAYERS] = {
 	"en",
 	"symbols",
 	"navigation",
 	"dialer",
 	"minimal",
-	"ru",
 };
 
 static Key* available_layers[LAYERS] = {
@@ -588,7 +524,6 @@ static Key* available_layers[LAYERS] = {
 	keys_navigation,
 	keys_dialer,
 	keys_minimal,
-	keys_ru
 };
 
 Buttonmod buttonmods[] = {
